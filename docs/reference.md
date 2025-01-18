ec8251a1db332080335586d25d080e6b6d005f41
Chun-Ho (Hugo) Lin <hugo970217@gmail.com>
2025-01-18 19:37:54 +0800

    feat: add LINE bot services and mock product data (#3)
    * chore: add @line/bot-sdk dependency
    
    * docs: add reference links to documentation
    
    * feat: add LINE bot services and mock product data

---
+++ docs/reference.md
@@ -0,0 +1,7 @@
+https://stackblitz.com/edit/react-ts-yadeuy?file=App.tsx
+
+https://github.com/ppppppsmash/liff-test-app
+
+https://hackmd.io/@Librarylai/BkP8diQnh
+
+https://github.com/librarylai/line-bot
---
+++ src/pages/api/lineExpireProductsBroadcast.js
@@ -0,0 +1,16 @@
+import { getExpireProductsListBroadcast } from '@/services/lineBotService'
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    /* 透過  broadcast 對群裡所有人進行商品到期通知 */
+    getExpireProductsListBroadcast()
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      .then((result) => res.status(200).end())
+      .catch((err) => {
+        console.log(err)
+        res.status(500).end()
+      })
+  } else {
+    res.status(405).end()
+  }
+}
